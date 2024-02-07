ee5e5c2e2e9fa271c58e5ace72bd33555e0d9dfc
Jose Eduardo VCN <114266650+JoseEduardoVelazquezCN@users.noreply.github.com>
2024-02-06 20:17:18 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -68,21 +68,3 @@ var swiper = new Swiper(".blogs-slider", {
 });
 
 
-//mandar al inicio de la pagina
-$(document).ready(function(){
-
-	$('.ir-arriba').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(300);
-		} else {
-			$('.ir-arriba').slideUp(300);
-		}
-	});
-
-});
